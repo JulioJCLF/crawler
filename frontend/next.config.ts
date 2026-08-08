2d7b59add08aed0fe3ce9c23e0e4f1ac5c2f30d0
Julio Lopes <juliolopes.bfg@gmail.com>
2026-08-08 14:43:52 -0300

    fix: next.js static export config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // O repositório no GitHub se chama crawler, então o base path do GitHub Pages será /crawler
+  basePath: "/crawler",
 };
 
 export default nextConfig;
